803b785926f7f808db2eb593470d6046cbcf8cab
lev <levv@192.168.1.13>
2022-01-09 11:53:26 +0200

    Improved CMD options handling

--- src/main/java/com/applitools/batchmapper/MFSBatchMapper.java
+++ src/main/java/com/applitools/batchmapper/MFSBatchMapper.java
@@ -4,9 +4,11 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.cli.*;
 import org.apache.pdfbox.pdmodel.PDDocument;
 
 import technology.tabula.ObjectExtractor;
@@ -24,173 +26,225 @@ import technology.tabula.extractors.BasicExtractionAlgorithm;
  *
  */
 public class MFSBatchMapper {
-	
-	public static int OFFSET = 2;
-   
 
 	public static void main(String[] args) throws IOException {
-		    
-		     if( args.length < 2 )
-		        {
-		            usage();
-		        }
-		        else
-		        {
-		        	MFSBatchMapper batchMapper = new MFSBatchMapper();
-		        	batchMapper.execute(args);
-		        }  
-		  
+
+		CommandLineParser parser = new DefaultParser();
+		Options options = getOptions();
+
+		try {
+			CommandLine cmd = parser.parse(options, args);
+
+			if (cmd.getOptions().length == 0)  {
+				usage(options);
+				return;
+			}
+
+			MFSBatchMapperExceuter batchMapper = new MFSBatchMapper().new MFSBatchMapperExceuter(cmd);
+			batchMapper.execute();
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-	  	  
-	    /**
-	     * Print help.
-	     */
-	    private static void usage()
-	    {
-	        System.err.println( "Usage: java -jar MFSBatchMapper.jar -f <A path to target folder or file>" );
-	    }
+	}
 
-	   
-	  public void execute(String[] args) throws IOException
-	  {
-		  String path = "";
-		  
-		  for (int i=0; i < args.length; i++) {
-	            if (args[i].equals("-f")) {
-	                i++;
-	                path = args[i];
-	            }
-	        }
-		  
-		  if(path == "") 
-		  {
-			  usage();
-			  return;
-		  }
-		    
-		  File root = new File(path);
-		  	if (root.isFile())
-		  	{
-		  		parseFile(root);
-		  	}
-		  	else
-		  	{
-		  		File[] files = root.listFiles();
-		  	   for (File file : files) {
-		  		 parseFile(file);
-	            }
-		  	}
-	  }
-	  
-	  public void parseFile(File file) throws IOException
-	  {
-		  ArrayList<String> TOC_Text = new ArrayList<String>();
-		  ArrayList<Integer> TOC_Pages = new ArrayList<Integer>();
-		    
-		    PDDocument pd = PDDocument.load(file);
+	private static Options getOptions() {
+		Options options = new Options();
 
-		    int totalPages = pd.getNumberOfPages();
-//		    System.out.println("Total Pages in Document: "+totalPages);
+		options.addOption(Option.builder("k")
+				.longOpt("apiKey")
+				.desc("Applitools Apikey")
+				.hasArg()
+				.argName("apikey")
+				.build());
 
-		    ObjectExtractor oe = new ObjectExtractor(pd);
-		    BasicExtractionAlgorithm sea = new BasicExtractionAlgorithm();
-		    Page page = oe.extract(2);
+		options.addOption(Option.builder("f")
+				.longOpt("folder")
+				.desc("Set the root folder to start the analysis, default: \\.")
+				.hasArg()
+				.argName("path")
+				.build());
 
-		    // extract text from the table after detecting
-		    List<Table> table = sea.extract(page);
-		    for(Table tables: table) {
-		        List<List<RectangularTextContainer>> rows = tables.getRows();
+		options.addOption(Option.builder("s")
+				.longOpt("server")
+				.desc("Set Applitools server url, default: https://mfseyes.applitools.com")
+				.hasArg()
+				.argName("url")
+				.build()
+				);
+		
+        options.addOption(Option.builder("fb")
+                .longOpt("flatbatch")
+                .desc("Aggregate all test results in a single batch (aka flat-batch), default: folder name ")
+                .hasArg()
+                .argName("name")
+                .build());
 
-		        for(int i=0; i<rows.size(); i++) {
 
-		            List<RectangularTextContainer> cells = rows.get(i);
+		return options;
 
-		            for(int j=0; j<cells.size(); j++) {
-		            	String text = cells.get(j).getText();
-		            	//System.out.println(text);
-		            	
-		            	if(Pattern.matches("[a-zA-Z ]+\\d+$", text) && text.length()>1)
-		            	{
-		            	try {
-		            		int len=text.length();
-		            		TOC_Text.add(text.substring(0, text.lastIndexOf(" ")==-1? text.length()-1 :text.lastIndexOf(" ")));
-          			            		
-		            		Pattern numbers = Pattern.compile("\\d+");
-		            		Matcher matcher = numbers.matcher(text);
-		            		if (matcher.find())
-		            		{
-		            		    TOC_Pages.add(Integer.parseInt(matcher.group(0)));
-		            		}
-		            		
-		            	}
-		            	catch (Exception e) {
-							// TODO: handle exception
-		            		System.out.println(e.getMessage());
+	}
+
+
+	/**
+	 * Print help.
+	 */
+	private static void usage(Options options)
+	{
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp("MFSBatch Mapper [-k <api-key>] [options]", options);
+	}
+
+	public class MFSBatchMapperExceuter
+	{
+
+		public int OFFSET = 2; // TOC + Header pages
+		final String batchId =UUID.randomUUID().toString(); 
+		
+		private CommandLine cmd;  
+		public MFSBatchMapperExceuter(CommandLine cmd)
+		{
+			this.cmd = cmd;
+		}
+
+		public void execute() throws IOException
+		{
+
+			File root = new File(cmd.getOptionValue("f", "."));
+			if (root.isFile())
+			{
+				parseFile(root);
+			}
+			else
+			{
+				File[] files = root.listFiles();
+				for (File file : files) {
+					parseFile(file);
+				}
+			}
+
+		} 
+
+
+		private void parseFile(File file) throws IOException
+		{
+			ArrayList<String> TOC_Text = new ArrayList<String>();
+			ArrayList<Integer> TOC_Pages = new ArrayList<Integer>();
+
+			PDDocument pd = PDDocument.load(file);
+
+			int totalPages = pd.getNumberOfPages();
+			//		    System.out.println("Total Pages in Document: "+totalPages);
+
+			ObjectExtractor oe = new ObjectExtractor(pd);
+			BasicExtractionAlgorithm sea = new BasicExtractionAlgorithm();
+			Page page = oe.extract(OFFSET);
+
+			// extract text from the table after detecting
+			List<Table> table = sea.extract(page);
+			for(Table tables: table) {
+				List<List<RectangularTextContainer>> rows = tables.getRows();
+
+				for(int i=0; i<rows.size(); i++) {
+
+					List<RectangularTextContainer> cells = rows.get(i);
+
+					for(int j=0; j<cells.size(); j++) {
+						String text = cells.get(j).getText();
+						//System.out.println(text);
+
+						if(Pattern.matches("[a-zA-Z ]+\\d+$", text) && text.length()>1)
+						{
+							try {
+								int len=text.length();
+								TOC_Text.add(text.substring(0, text.lastIndexOf(" ")==-1? text.length()-1 :text.lastIndexOf(" ")));
+
+								Pattern numbers = Pattern.compile("\\d+");
+								Matcher matcher = numbers.matcher(text);
+								if (matcher.find())
+								{
+									TOC_Pages.add(Integer.parseInt(matcher.group(0)));
+								}
+
+							}
+							catch (Exception e) {
+								// TODO: handle exception
+								System.out.println(e.getMessage());
+							}
+
+
 						}
-	       
-		            		            		
-		            	}
-		            	
-		     
-	            	
-		            }
 
-		        }
-		    }
-		    pd.close();
-		    generateIT(TOC_Text,CalcPages(TOC_Pages,totalPages),file);
 
-	  }
-	  
-	  
-	  
-	  
-	  public ArrayList<String> CalcPages(ArrayList<Integer> pages,int totalPages)
-	  {
-		    ArrayList<String> TOC_pages = new ArrayList<String>();
-		    int loopSize= pages.size()-1;
-		    int i,a=0,b;
-		    	
-		    for(i=0;i<loopSize;i++)
-		    {
-		    	a=pages.get(i);
-		    	b=pages.get(i+1);
-		    	
-		    	if(b-a==1)
-		    	{
-		    		TOC_pages.add(i, ""+a);
-		    	}
-		    	else
-		    		TOC_pages.add(i, String.format("%d-%d", a,b-1));
-		    }
 
-		    totalPages=totalPages-OFFSET;
-		    a=pages.get(i);
-		    
-	    	if(totalPages-a==0)
-	    	{
-	    		TOC_pages.add(i, ""+a);
-	    	}
-	    	else
-	    		TOC_pages.add(i, String.format("%d-%d", a,totalPages));
-		   
-	   return TOC_pages;
-		  
-	  }
-	  
-	  public void generateIT(ArrayList<String> text, ArrayList<String> pages,File file) throws IOException
-	  {
-		  int i;
-		  String str="";
-		  String pattern = "java -jar ~/Documents/ImageTester_2.0.0.jar -f \"%s\" -a \"%s\" -fn \"%s\" -sp %s -fb \"TOC1<>MAP1\" && \n";		  
-		  for(i=0;i<text.size();i++)
-		  {
-			 
-			  str+= String.format(pattern,file.getCanonicalPath(),file.getName(), text.get(i),pages.get(i));
-			  
-			  }
-		  System.out.println(str);
-		  
-	  }
-	  
+					}
+
+				}
+			}
+			pd.close();
+			generateIT(TOC_Text,CalcPages(TOC_Pages,totalPages),file);
+
+		}
+
+
+
+
+		private ArrayList<String> CalcPages(ArrayList<Integer> pages,int totalPages)
+		{
+			ArrayList<String> TOC_pages = new ArrayList<String>();
+			int loopSize= pages.size()-1;
+			int i,a=0,b;
+
+			for(i=0;i<loopSize;i++)
+			{
+				a=pages.get(i);
+				b=pages.get(i+1);
+
+				if(b-a==1)
+				{
+					TOC_pages.add(i, ""+a);
+				}
+				else
+					TOC_pages.add(i, String.format("%d-%d", a,b-1));
+			}
+
+			totalPages=totalPages-OFFSET;
+			a=pages.get(i);
+
+			if(totalPages-a==0)
+			{
+				TOC_pages.add(i, ""+a);
+			}
+			else
+				TOC_pages.add(i, String.format("%d-%d", a,totalPages));
+
+			return TOC_pages;
+
+		}
+
+		public void generateIT(ArrayList<String> text, ArrayList<String> pages,File file) throws IOException
+		{
+			int i;
+			
+			String str="";
+            String apiKey = cmd.getOptionValue("k","");
+            String serverUrl = cmd.getOptionValue("s", "https://mfseyes.applitools.com");
+            String batchName = cmd.getOptionValue("fb", file.getParent().substring(file.getParent().lastIndexOf(File.separator)+1));
+            
+            
+            apiKey = apiKey =="" ? "" : "-k " + apiKey;
+            serverUrl = serverUrl =="" ? "" : "-s " + "\""+serverUrl+"\"";
+            
+			String pattern = "java -jar ImageTester_2.2.1 %s %s -f \"%s\" -a \"%s\" -fn \"%s\" -sp %s -fb \"%s<>%s\" && \n";		  
+			for(i=0;i<text.size();i++)
+			{
+
+				str+= String.format(pattern,apiKey,serverUrl,file.getCanonicalPath(),file.getName(), text.get(i),pages.get(i),batchName,batchId);
+
+			}
+			System.out.println(str);
+
+		}
+	}
 }
